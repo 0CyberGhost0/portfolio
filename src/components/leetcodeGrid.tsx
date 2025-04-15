76532f98a1063cfa2a3f5acce1fe99712cb02a06
0CyberGhost0 <prakashved0702@gmail.com>
2025-04-16 02:26:56 +0530

    deploy ready

--- src/components/leetcodeGrid.tsx
+++ src/components/leetcodeGrid.tsx
@@ -1,25 +1,7 @@
 
 import Image from 'next/image';
 export default function LeetCodeGrid() {
-    const exampleTheme = {
-        light: [
-            'rgb(235, 235, 235)',
-            'rgba(192, 132, 245, 0.44)',
-            'rgba(192, 132, 245, 0.6)',
-            'rgba(192, 132, 245, 0.76)',
-            'rgba(192, 132, 245, 0.92)',
-        ],
-        dark: [
-            'rgb(235, 235, 235)',
-            'rgba(192, 132, 245, 0.44)',
-            'rgba(192, 132, 245, 0.6)',
-            'rgba(192, 132, 245, 0.76)',
-            'rgba(192, 132, 245, 0.92)',
-        ],
-    }
-
     return (
-
         <img
             src="https://leetcard.jacoblin.cool/Ved07?ext=heatmap"
             alt="LeetCode Stats"
